9d0fd0364f145fba96f641b693cb6ac4233f69f5
Cedric Han <cedric@tatari.tv>
2017-08-03 10:21:05 -0700

    Update download link for geolite2 db

--- geolite2/download.sh
+++ geolite2/download.sh
@@ -2,13 +2,16 @@
 
 cd _geoip_geolite2
 rm *.mmdb
-wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz
-gunzip "GeoLite2-City.mmdb.gz"
+wget http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.tar.gz
+tar zxf "GeoLite2-City.tar.gz"
+mv GeoLite2-City_*/*.mmdb .
+rm -rf GeoLite2-City_*
+rm GeoLite2-City.tar.gz
 cd ..
 
 export PYTHONPATH=`pwd`:`pwd`/..
 python -c "if 1:
     from geoip import geolite2;
     info = geolite2.get_info()
-    print info.date.strftime('%Y.%m%d')
+    print(info.date.strftime('%Y.%m%d'))
 " > VERSION
